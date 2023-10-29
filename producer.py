5e477f0318b68ca1e6aa333b5301ef3a549fffca
tngtied <tngtied@gmail.com>
2023-10-29 16:27:44 +0900

    producer.py 중간 완성

--- producer.py
+++ producer.py
@@ -1,11 +1,18 @@
 import socket, time, sys
-sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = sys.argv[1]
 port = int(sys.argv[2])
+
+sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sc.connect((host, port))
 
 while True:
     events = input()
+    print("%d events are created" % len(events))
+
     sc.send(events.encode())
+    if (KeyboardInterrupt):
+        print("exit")
+        sys.exit
--- server.py
+++ server.py
@@ -6,13 +6,27 @@ host = sys.argv[1]
 port_a = int(sys.argv[2])
 port_b = int(sys.argv[3])
 
-data_queue = Queue
+events_queue = Queue()
 
 print(sys.argv)
 
-def producer_worker():
+def producer_worker(sc):
+    while True:
+        events = sc.accept()
+        print("[Events created]")
+        if not events:
+            break
+        ##이거 괜찮은 거 맞나?
+        for index in range(0, len(events)):
+            events_queue.put(events[index])
+    ##producer 단위 확인용 iteration
+    print("in queue: ")
+    for event in iter(events_queue.get, None):
+        print("  %c" % event)
+        
 
-def consumer_worker(...): ...
+
+# def consumer_worker(...): ...
 
 if (__name__ == '__main__'):
     producer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,19 +35,19 @@ if (__name__ == '__main__'):
     producer_socket.listen(5)    
 
     producer_socket_, producer_addr = producer_socket.accept()
-
-    producer_thread = Thread(target = producer_worker) 
+    print("[Producer connected]")
+    producer_thread = Thread(target = producer_worker, args=(producer_socket, )) 
     producer_thread.start()
 
-    consumer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    consumer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    consumer_socket.bind((host, port_b))
-    consumer_socket.listen(5)
+    # consumer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # consumer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # consumer_socket.bind((host, port_b))
+    # consumer_socket.listen(5)
 
-    consumer_group = []
-    consumer_count = 0
-    while True: 
-        conn
+    # consumer_group = []
+    # consumer_count = 0
+    # while True: 
+    #     conn
 
-        worker_thread = Thread(target = consumer_worker, args=(,))
-        worker_thread.start()
+    #     worker_thread = Thread(target = consumer_worker, args=(,))
+    #     worker_thread.start()
